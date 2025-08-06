ca91b26ae962c051a526cad1d299412aaa59becb
RohitSah23 <rohitisog@gmail.com>
2025-08-06 17:32:59 +0530

    feat (Courses): Added Courses Section to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import WhyMalta from "@/components/WhyMalta/WhyMalta";
 import Services from "@/components/Services/Services";
+import Courses from "@/components/Courses/Courses";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <Hero/>
     <WhyMalta/>
     <Services/>
+    <Courses/>
     <Footer/>
    </div>
   );
